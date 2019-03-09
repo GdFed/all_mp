c6703f43382b452f7a734c77de172281b30324b2
xiaozhilong <xiaozhilong@huishoubao.com.cn>
2019-03-09 16:18:11 +0800

    meta

--- meta.js
+++ meta.js
@@ -1,12 +1,3 @@
-const path = require('path')
-const fs = require('fs')
-const {
-  sortDependencies,
-  installDependencies,
-  runLintFix,
-  printMessage,
-} = require('./utils')
-
 module.exports = {
   helpers: {
     if_or: function(v1, v2, options) {
@@ -18,56 +9,6 @@ module.exports = {
     },
   },
   prompts: {
-    name: {
-      type: 'string',
-      required: true,
-      message: 'Project name',
-    },
-    description: {
-      type: 'string',
-      required: false,
-      message: 'Project description',
-      default: 'A Vue.js project',
-    },
-    author: {
-      type: 'string',
-      message: 'Author',
-    }
-  },
-  filters: {
-    '.eslintrc.js': 'lint',
-    '.eslintignore': 'lint',
-    'config/test.env.js': 'unit || e2e',
-    'build/webpack.test.conf.js': "unit && runner === 'karma'",
-    'test/unit/**/*': 'unit',
-    'test/unit/index.js': "unit && runner === 'karma'",
-    'test/unit/jest.conf.js': "unit && runner === 'jest'",
-    'test/unit/karma.conf.js': "unit && runner === 'karma'",
-    'test/unit/specs/index.js': "unit && runner === 'karma'",
-    'test/unit/setup.js': "unit && runner === 'jest'",
-    'test/e2e/**/*': 'e2e',
-    'src/router/**/*': 'router',
-  },
-  complete: function(data, { chalk }) {
-    const green = chalk.green
-
-    sortDependencies(data, green)
-
-    const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
-
-    if (data.autoInstall) {
-      installDependencies(cwd, data.autoInstall, green)
-        .then(() => {
-          return runLintFix(cwd, data, green)
-        })
-        .then(() => {
-          printMessage(data, green)
-        })
-        .catch(e => {
-          console.log(chalk.red('Error:'), e)
-        })
-    } else {
-      printMessage(data, chalk)
-    }
-  },
+    completeMessage: `请按以下步骤启动，耐心等待:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm i\n  npm run dev 或者 npm start`
+  }
 }
